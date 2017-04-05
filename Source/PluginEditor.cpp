40337d430b0e2cb72f587e4c2aa2ed072eb4a59c
Dino Pollano <cpollano@gmail.com>
2017-04-06 00:12:06 +0100

    spun round and drawing in pixels
    Spectrum is now drawn from bottom of the screen up, and instead of vertical lines the spectrum is now painted using individual pixels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -14,50 +14,44 @@
 //==============================================================================
 SpectrumAudioProcessorEditor::SpectrumAudioProcessorEditor (
     SpectrumAudioProcessor& p)
-    : AudioProcessorEditor (&p),
-      processor (p),
-      spectrumHeight (0)
+    : AudioProcessorEditor (&p), processor (p), spectrumHeight (0)
 {
 	// Make sure that before the constructor has finished, you've set the
 	// editor's size to whatever you need it to be.
 	setSize (1024, 300);
-  startTimer(100);
-
+	startTimer (100);
 }
 
 SpectrumAudioProcessorEditor::~SpectrumAudioProcessorEditor () {}
 //==============================================================================
 void SpectrumAudioProcessorEditor::paint (Graphics& g)
 {
-	g.fillAll (Colours::white);
-	
-  std::vector<float> s = processor.getSPectrum();
-  std::transform(s.begin(), s.end(), s.begin(), std::bind1st(std::multiplies<float>(), spectrumHeight));
-  g.setColour (Colours::black);
-  for( size_t i = 0; i < numPoints; i++)
-  {
-	g.drawLine(xCords[i], 0.,xCords[i], s[i]  ,1.f);
-    
-  }
-  
+	g.fillAll (Colours::black);
+
+	std::vector<float> s = processor.getSPectrum ();
+	std::transform (s.begin (), s.end (), s.begin (),
+	                std::bind1st (std::multiplies<float> (), spectrumHeight));
+	g.setColour (Colours::grey);
+	for (size_t i = 0; i < numPoints; i++)
+	{
+//		g.drawLine (xCords[i], originY, xCords[i], originY - s[i], 1.f);
+    g.setPixel(xCords[i],  originY - s[i]);
+	}
 }
 void SpectrumAudioProcessorEditor::resized ()
 {
-  numPoints = processor.getFFtSize ()/2;
-  xCords.assign (numPoints, 0.);
-	auto bounds    = getLocalBounds ();
-	spectrumHeight = bounds.getHeight () - 10;
-	spectrumWidth  = bounds.getWidth () - 10;
+	Rectangle<int> r (getLocalBounds ().reduced (8));
+
+	numPoints = processor.getFFtSize () / 4;
+	xCords.assign (numPoints, 0.);
+	spectrumHeight = r.getHeight ();
+	spectrumWidth  = r.getWidth ();
 	spacing        = spectrumWidth / numPoints;
 	std::iota (xCords.begin (), xCords.end (), 1);
-  std::transform(xCords.begin(), xCords.end(), xCords.begin(), std::bind1st(std::multiplies<float>(),spacing));
-	originY = bounds.getHeight () - 10;
-  
-	// This is generally where you'll want to lay out the positions of any
-	// subcomponents in your editor..
+	std::transform (xCords.begin (), xCords.end (), xCords.begin (),
+	                std::bind1st (std::multiplies<float> (), spacing));
+	originY = r.getHeight ();
+
 }
 
-void SpectrumAudioProcessorEditor::timerCallback()
-{
-  repaint();
-}
+void SpectrumAudioProcessorEditor::timerCallback () { repaint (); }
